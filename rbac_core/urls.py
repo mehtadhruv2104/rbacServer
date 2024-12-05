5afa66127a2458f4440311774720b13e9e974f75
mehtadhruv2104 <mehtadhruv498@gmail.com>
2024-12-05 19:50:31 +0530

    Added API's for RBAC Use Cases
    Added the routing logic. Added Models for the Entities. Added Serializers to convert object to json. Added views for the main logic and response

---
+++ rbac_core/urls.py
@@ -0,0 +1,14 @@
+# from django.urls import path
+# from .views import RoleViewSet, UserViewSet, PermissionViewSet, AuditLogViewSet
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet,PermissionViewSet,RolesViewSet,SpecificRolesPermissionViewSet,ValidateUserViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'users/validate', ValidateUserViewSet, basename='user/validate')
+router.register(r'permissions', PermissionViewSet, basename='permission')
+router.register(r'roles', RolesViewSet, basename='roles')
+router.register(r'roles/permissions/(?P<role_name>\w+)', SpecificRolesPermissionViewSet, basename='roles/permissions')
+
+urlpatterns += router.urls
